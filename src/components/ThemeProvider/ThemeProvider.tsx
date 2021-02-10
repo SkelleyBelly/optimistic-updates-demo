5fce8a253f64cc0dd86e1fb6a939c8bfddbcab00
Nathan Skelley <nathan.skelley@bath.edu>
2021-02-10 13:23:11 +0000

    Added Material UI and configured theme

---
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,48 @@
+import {
+  createMuiTheme,
+  ThemeProvider as MuiThemeProvider,
+  ThemeProviderProps,
+  NoSsr,
+  CssBaseline,
+  Theme,
+} from "@material-ui/core";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Quicksand", "Helvetica", "Arial", sans-serif',
+  },
+  spacing: 4,
+  shape: {
+    borderRadius: 12,
+  },
+  palette: {
+    primary: {
+      light: "#FFECFD",
+      main: "#C31CB5",
+    },
+    text: {
+      primary: "#303030",
+      secondary: "rgba(0,0,0,0.5)",
+    },
+    background: {
+      default: "#FFFDF8",
+      paper: "#FFFCF2",
+    },
+    divider: "rgba(0,0,0,0.05)",
+  },
+  shadows: [
+    "none",
+    "0px 5px 10px 5px rgba(0, 0, 0, 0.05)",
+    ...Array(23).fill('none')
+  ] as Theme['shadows']
+});
+
+const ThemeProvider = ({ children }: Omit<ThemeProviderProps, "theme">) => (
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline>
+      <NoSsr>{children}</NoSsr>
+    </CssBaseline>
+  </MuiThemeProvider>
+);
+
+export default ThemeProvider;
